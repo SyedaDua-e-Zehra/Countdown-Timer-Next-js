a8fd6cedea9e41c16cbebe7c34540c69b222e8a8
Syeda Dua e Zehra <syedaduaezehrar@gmail.com>
2024-11-29 09:27:39 -0800

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Countdown from "@/components/countdown";
+
+// extension download krni hai ES7 KI
+export default function Home (){
+    return(
+        <div>
+        <Countdown />
+        </div>
+    )
+}
